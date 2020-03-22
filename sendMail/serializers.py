03e407859f14300bcd948bbfedc27a8ba847abe0
damionjestem <ndnd@protonmail.ch>
2020-03-22 19:39:15 +0100

    add Django Project

---
+++ sendMail/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import sendMail
+
+class MailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = sendMail
+        fields = ('id', 'name', 'paradigm')
---
+++ sendMail/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import sendMail
+from .serializers import MailSerializer
+
+class MailView(viewsets.ModelViewSet):
+    queryset = sendMail.objects.all()
+    serializer_class = MailSerializer
